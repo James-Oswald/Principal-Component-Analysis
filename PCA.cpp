9a37ab06c51849f4159ddab642d249e1c77f8695
James-Oswald <jamesoswald111@gmail.com>
2020-08-08 21:47:42 -0400

    working on PCA

--- PCA.cpp
+++ PCA.cpp
@@ -6,6 +6,12 @@
 #define CSV_IO_NO_THREAD
 #include "csv.h"
 
+double sampleMean(arma::colvec attributeArray){
+    return arma::mean(attributeArray);
+}
+
+
+
 arma::mat readFile(const char* fileName){
     arma::mat rv(150, 3, arma::fill::zeros);
     io::CSVReader<5> reader(fileName);
@@ -18,18 +24,20 @@ arma::mat readFile(const char* fileName){
 
 arma::mat PCA(arma::mat D, double alpha){
     double n = D.n_rows, d = D.n_cols;
-    arma::mat sigma = arma::cov(D);
+    arma::mat sigma = arma::cov(D, 1);
+    sigma.save("./armaMatrixDump/covariance.csv", arma::csv_ascii);
     arma::cx_vec eigval;
     arma::cx_mat eigvec;
     arma::eig_gen(eigval, eigvec, sigma, "balance");
-    eigval.save("./armaMatrixDump/eigenVals.csv", arma::csv_ascii);
-    eigvec.save("./armaMatrixDump/eigenVecs.csv", arma::csv_ascii);
+    //eigval.save("./armaMatrixDump/eigenVals.csv", arma::csv_ascii);
+    //eigvec.save("./armaMatrixDump/eigenVecs.csv", arma::csv_ascii);
     return D;
 }
 
 int main(){
     try{
         arma::mat dataMatrix = readFile("iris.txt");
+        //dataMatrix.save("./armaMatrixDump/dataMatrix.csv", arma::csv_ascii);
         PCA(dataMatrix, 0.95);
     }catch(std::exception& e){
         std::cout<<e.what()<<"\n";
